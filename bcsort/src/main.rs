ae8e1fa344dce540904791c686366f0daa7b3f4b
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-02-27 16:51:49 +0000

    Scoped threads

--- bcsort/src/main.rs
+++ bcsort/src/main.rs
@@ -1,3 +1,4 @@
+use bytelines;
 use clap::Parser;
 use ext_sort::{buffer::LimitedBufferBuilder, ExternalSorter, ExternalSorterBuilder};
 use num_cpus;
@@ -5,9 +6,9 @@ use parse_size::parse_size;
 use std::fs::File;
 use std::io::{self, prelude::*};
 use std::path;
+use std::path::PathBuf;
 use std::sync::mpsc::{self, Sender};
 use std::thread;
-use bytelines;
 
 /// Extract and sort blockchair data
 #[derive(Parser, Debug)]
@@ -28,65 +29,76 @@ struct Args {
     skip: usize,
 }
 
-fn process_chunk(chunk : Vec<std::path::PathBuf>, sender: Sender<String>, buffer_size: usize, fields: std::sync::Arc<Vec<usize>>, skip: usize) {
+fn process_chunk(
+    chunk: &[PathBuf],
+    sender: Sender<String>,
+    buffer_size: usize,
+    fields: &[usize],
+    skip: usize,
+) {
     let (thread_sender, thread_receiver) = mpsc::sync_channel(1000000);
 
-    let handler = thread::spawn(move || {
-        for dir_entry in chunk {
-            let file = File::open(dir_entry).unwrap();
-            let reader = io::BufReader::with_capacity(1024 * 1024, file);
-            let mut byte_reader = bytelines::ByteLines::new(reader);
+    thread::scope(|scope| {
+        scope.spawn(move || {
+            for dir_entry in chunk {
+                let file = File::open(dir_entry).unwrap();
+                let reader = io::BufReader::with_capacity(1024 * 1024, file);
+                let mut byte_reader = bytelines::ByteLines::new(reader);
 
-            for _ in 0..skip { 
-                byte_reader.next();
-            }
-
-            while let Some(line) = byte_reader.next() {
-                let mut tab_pos = Vec::with_capacity(16);
-                let mut chunks = Vec::with_capacity(fields.len());
-
-                let s = line.unwrap();
-                let mut last = 0;
-                tab_pos.push(0);
-                while let Some(pos) = s[last + 1..].iter().position(|x| *x == b'\t') {
-                    last += pos + 2;
-                    tab_pos.push(last);
+                for _ in 0..skip {
+                    byte_reader.next();
                 }
 
-                for f in fields.iter() {
-                    chunks.push(&s[tab_pos[*f]..tab_pos[*f + 1] - 1]);
+                while let Some(line) = byte_reader.next() {
+                    let mut tab_pos = Vec::with_capacity(16);
+                    let mut chunks = Vec::with_capacity(fields.len());
+
+                    let s = line.unwrap();
+                    let mut last = 0;
+                    tab_pos.push(0);
+                    while let Some(pos) = s[last + 1..].iter().position(|x| *x == b'\t') {
+                        last += pos + 2;
+                        tab_pos.push(last);
+                    }
+
+                    for f in fields.iter() {
+                        chunks.push(&s[tab_pos[*f]..tab_pos[*f + 1] - 1]);
+                    }
+
+                    // Send/sort as String
+                    thread_sender
+                        .send(String::from_utf8(chunks.join(&b'\t')).unwrap())
+                        .unwrap();
+                    // Send/sort as Vec<u8>
+                    // thread_sender.send(chunks.join(&b'\t')).unwrap();
                 }
-
-                // Send/sort as String
-                thread_sender.send(String::from_utf8(chunks.join(&b'\t')).unwrap()).unwrap();
-                // Send/sort as Vec<u8>
-                // thread_sender.send(chunks.join(&b'\t')).unwrap();
             }
-        }
-    });
+        });
 
-    let sorter: ExternalSorter<_, io::Error, LimitedBufferBuilder> =
-        ExternalSorterBuilder::new()
-            .with_tmp_dir(path::Path::new("/tmp"))
-            .with_buffer(LimitedBufferBuilder::new(buffer_size, true))
-            .with_threads_number(num_cpus::get())
-            .build()
+        let sorter: ExternalSorter<_, io::Error, LimitedBufferBuilder> =
+            ExternalSorterBuilder::new()
+                .with_tmp_dir(path::Path::new("/tmp"))
+                .with_buffer(LimitedBufferBuilder::new(buffer_size, true))
+                .with_threads_number(num_cpus::get())
+                .build()
+                .unwrap();
+
+        let sorted = sorter
+            .sort(thread_receiver.into_iter().map(|x| Ok(x)))
             .unwrap();
 
-    let sorted = sorter.sort(thread_receiver.into_iter().map(|x| Ok(x))).unwrap();
-
-/*    let ln = &[b'\n'];
-    for item in sorted.map(Result::unwrap) {
-        std::io::stdout().write_all(item.as_bytes()).unwrap();
-        std::io::stdout().write(ln).unwrap();
-    }
-    std::io::stdout().flush().unwrap();*/
-    sorted.count();
-    handler.join().unwrap();
+        let ln = &[b'\n'];
+        for item in sorted.map(Result::unwrap) {
+            std::io::stdout().write_all(item.as_bytes()).unwrap();
+            std::io::stdout().write(ln).unwrap();
+        }
+        std::io::stdout().flush().unwrap();
+        // sorted.count();
+    });
 }
+
 fn main() {
     let args = Args::parse();
-    let fields = std::sync::Arc::new(args.fields);
     let buffer_size = parse_size(args.buffer_size).expect("Wrong format for buffer size") as usize;
 
     let files = args
@@ -97,21 +109,17 @@ fn main() {
         .map(|d| d.path())
         .collect::<Vec<std::path::PathBuf>>();
 
-    let mut thread_handles = vec![];
-
     let mut receivers: Vec<std::sync::mpsc::Receiver<String>> = vec![];
 
-    for chunk in files.chunks(num_cpus::get()) {
-        let (sender, receiver) = mpsc::channel();
-        receivers.push(receiver);
-        let fields = fields.clone();
-        let chunk = chunk.to_owned();
-        thread_handles.push(thread::spawn(move || {
-            process_chunk(chunk, sender, buffer_size, fields, args.skip);
-        }));
-    }
-    for handle in thread_handles {
-        handle.join().unwrap();
-    }
+    let fields = &args.fields;
 
+    thread::scope(|scope| {
+        for chunk in files.chunks(num_cpus::get()) {
+            let (sender, receiver) = mpsc::channel();
+            receivers.push(receiver);
+            scope.spawn(move || {
+                process_chunk(chunk, sender, buffer_size, fields, args.skip);
+            });
+        }
+    });
 }
